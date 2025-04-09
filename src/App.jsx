d053710896152ee423cec1f2e61440568f10a073
Tanushree Mohanty <mohantytanushree234@example.com>
2025-04-09 19:25:37 +0530

    initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// src/App.jsx
+import { useState } from "react";
+import { Container, Typography, CssBaseline } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography variant="h4" align="center" gutterBottom>
+          Weather App
+        </Typography>
+        {/* Features will be added here step by step */}
+      </Container>
+    </>
+  );
+}
+
+export default App;
